f7ca99eeb0563d14d2d754edebeeb76c11180e9b
Abhishek Prajapati <84218124+BytesAbhi@users.noreply.github.com>
2025-08-12 13:30:49 +0530

    Update 2025_08_09_144444_create_regusers_table.php

--- database/migrations/2025_08_09_144444_create_regusers_table.php
+++ database/migrations/2025_08_09_144444_create_regusers_table.php
@@ -14,7 +14,9 @@ return new class extends Migration
         Schema::create('regusers', function (Blueprint $table) {
             $table->id();
             $table->string('name');
+            $table->string('username')->unique();
             $table->string('email')->unique();
+            $table->string('phone');
             $table->string('password');
             $table->string('selfie_url')->nullable();
             $table->timestamps();
